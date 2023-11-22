b73783cb49d2abcd59d8f40eb7c2e5ce47d3e7e6
yasindere <yasin.dere58@hotmail.com>
2023-11-22 23:21:13 +0300

    Logger implementation

---
+++ src/Services/.BaseService/AppyNox.Services.Base.Application/Helpers/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace AppyNox.Services.Base.Application.Helpers;
+
+public static class StringExtensions
+{
+    #region Public Methods
+
+    public static string MinifyLogData(this string data)
+    {
+        return data.Replace("\\r", "").Replace("\\n", "").Replace("\\t", "").Replace("\\\"", "\"");
+    }
+
+    #endregion
+}
---
+++ src/Services/.BaseService/AppyNox.Services.Base.Domain/Common/RequestLogModel.cs
@@ -0,0 +1,30 @@
+﻿namespace AppyNox.Services.Base.Domain.Common;
+
+public class RequestLogModel
+{
+    #region Public Constructors
+
+    public RequestLogModel(string method, string path, string? queryString, string? body)
+    {
+        Method = method;
+        Path = path;
+        QueryString = queryString;
+        Body = body;
+    }
+
+    #endregion
+
+    #region Properties
+
+    public string Method { get; set; }
+
+    public string Path { get; set; }
+
+    public string? QueryString { get; set; }
+
+    public string? Body { get; set; }
+
+    #endregion
+
+    // others .. if needed
+}
