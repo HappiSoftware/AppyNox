d8aed29594adb2a34d709c71d1dff161806573fb
yasindere <yasin.dere58@hotmail.com>
2023-11-28 13:30:34 +0300

    SSL configuration for Ocelot

--- src/Gateways/AppyNox.Gateway.OcelotGateway/Program.cs
+++ src/Gateways/AppyNox.Gateway.OcelotGateway/Program.cs
@@ -12,6 +12,29 @@ try
 {
     var builder = WebApplication.CreateBuilder(args);
 
+    #region [ SSL Configuration ]
+
+    builder.WebHost.ConfigureKestrel(serverOptions =>
+    {
+        string fileName = string.Empty;
+
+        if (builder.Environment.IsDevelopment())
+        {
+            fileName = Directory.GetCurrentDirectory() + "/ssl/gateway.pfx";
+        }
+        else if (builder.Environment.IsProduction())
+        {
+            fileName = "/https/gateway.pfx";
+        }
+
+        serverOptions.ConfigureEndpointDefaults(listenOptions =>
+        {
+            listenOptions.UseHttps(fileName ?? throw new InvalidOperationException("SSL certificate file path could not be determined."), "happi2023");
+        });
+    });
+
+    #endregion
+
     builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
         .AddJsonFile($"Configurations/ocelot.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
         .AddEnvironmentVariables();
@@ -27,9 +50,9 @@ try
 
     var app = builder.Build();
 
-    await app.UseOcelot();
-
     app.UseMiddleware<LoggingMiddleware>();
+    
+    await app.UseOcelot();
 
     app.Run();
 }
