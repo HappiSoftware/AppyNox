722f989272fb6f110880c4d1fbb3072513637cdf
yasindere <yasin.dere58@hotmail.com>
2023-12-09 13:36:07 +0300

    AppyNox-57 EventBus Tests
    -EventBus, Manager, RabbitMQ unit tests completed
    
    closes #57

---
+++ src/BuildingBlocks/Tests/UnitTests/AppyNox.BuildingBlocks.EventBus.UnitTest/RabbitmqTests/EventBusRabbitmqBaseTests.cs
@@ -0,0 +1,61 @@
+﻿using AppyNox.EventBus.Base;
+using AppyNox.EventBus.RabbitMQ;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AppyNox.BuildingBlocks.EventBus.UnitTest.RabbitmqTests
+{
+    public class EventBusRabbitmqBaseTests
+    {
+        #region Fields
+
+        private ServiceProvider _sp;
+
+        #endregion
+
+        #region Public Constructors
+
+        public EventBusRabbitmqBaseTests()
+        {
+            ServiceCollection _services = new ServiceCollection();
+            _sp = _services.BuildServiceProvider();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        [Fact]
+        public void EventNameShouldBeReturnedAsBaseName()
+        {
+            EventBusConfig config = new()
+            {
+                EventNamePrefix = "Prefix",
+                EventNameSuffix = "Suffix"
+            };
+
+            var bus = new EventBusRabbitMQ(config, _sp);
+
+            var baseEventName = bus.ProcessEventName("PrefixTestBaseEventSuffix");
+
+            Assert.Equal("TestBaseEvent", baseEventName);
+        }
+
+        [Fact]
+        public void SubNameShouldReturnWithClientAppNameAndBaseEventName()
+        {
+            EventBusConfig config = new()
+            {
+                SubscriberClientAppName = "EventBus.UnitTests",
+                EventNameSuffix = "IntegrationEvent"
+            };
+
+            var bus = new EventBusRabbitMQ(config, _sp);
+
+            var subName = bus.GetSubName("TestCreatedIntegrationEvent");
+
+            Assert.Equal("EventBus.UnitTests.TestCreated", subName);
+        }
+
+        #endregion
+    }
+}
---
+++ src/BuildingBlocks/Tests/UnitTests/AppyNox.BuildingBlocks.EventBus.UnitTest/SubscriptionManagerTests/InMemoryEventBusSubscriptionManagerTests.cs
@@ -0,0 +1,94 @@
+﻿using AppyNox.BuildingBlocks.EventBus.UnitTest.Events;
+using AppyNox.EventBus.Base.Abstraction;
+using AppyNox.EventBus.Base.SubscriptionManagers;
+
+namespace AppyNox.BuildingBlocks.EventBus.UnitTest.SubscriptionManagerTests
+{
+    public class InMemoryEventBusSubscriptionManagerTests
+    {
+        #region Public Methods
+
+        [Fact]
+        public void GetEventKeyShouldReturnFormatted()
+        {
+            // Arrange
+            var subscriptionManager = new InMemoryEventBusSubscriptionManager(e => e.TrimEnd("IntegrationEvent".ToArray()));
+            var eventName = "TestCreated";
+
+            // Act
+            var eventKey = subscriptionManager.GetEventKey<TestCreatedIntegrationEvent>();
+
+            // Assert
+            Assert.Equal(eventName, eventKey);
+        }
+
+        [Fact]
+        public void AddSubscriptionShouldAddHandler()
+        {
+            // Arrange
+            var subscriptionManager = new InMemoryEventBusSubscriptionManager(e => e);
+            var eventName = "TestCreatedIntegrationEvent";
+
+            // Act
+            subscriptionManager.AddSubscription<TestCreatedIntegrationEvent, IIntegrationEventHandler<TestCreatedIntegrationEvent>>();
+
+            // Assert
+            Assert.True(subscriptionManager.HasSubscriptionsForEvent(eventName));
+        }
+
+        [Fact]
+        public void RemoveSubscriptionShouldRemoveHandler()
+        {
+            // Arrange
+            var subscriptionManager = new InMemoryEventBusSubscriptionManager(e => e);
+            var eventName = "TestCreatedIntegrationEvent";
+            subscriptionManager.AddSubscription<TestCreatedIntegrationEvent, IIntegrationEventHandler<TestCreatedIntegrationEvent>>();
+
+            // Act
+            subscriptionManager.RemoveSubscription<TestCreatedIntegrationEvent, IIntegrationEventHandler<TestCreatedIntegrationEvent>>();
+
+            // Assert
+            Assert.False(subscriptionManager.HasSubscriptionsForEvent(eventName));
+        }
+
+        [Fact]
+        public void GetHandlersForEventShouldReturnHandlers()
+        {
+            // Arrange
+            var subscriptionManager = new InMemoryEventBusSubscriptionManager(e => e);
+            subscriptionManager.AddSubscription<TestCreatedIntegrationEvent, IIntegrationEventHandler<TestCreatedIntegrationEvent>>();
+
+            // Act
+            var handlers = subscriptionManager.GetHandlersForEvent<TestCreatedIntegrationEvent>();
+
+            // Assert
+            Assert.Single(handlers);
+        }
+
+        [Fact]
+        public void HasSubscriptionsForEventWhenNoSubscriptionsShouldReturnFalse()
+        {
+            // Arrange
+            var subscriptionManager = new InMemoryEventBusSubscriptionManager(e => e);
+
+            // Act & Assert
+            Assert.False(subscriptionManager.HasSubscriptionsForEvent<TestCreatedIntegrationEvent>());
+        }
+
+        [Fact]
+        public void ClearShouldRemoveAllHandlers()
+        {
+            // Arrange
+            var subscriptionManager = new InMemoryEventBusSubscriptionManager(e => e);
+            subscriptionManager.AddSubscription<TestCreatedIntegrationEvent, IIntegrationEventHandler<TestCreatedIntegrationEvent>>();
+
+            // Act
+            subscriptionManager.Clear();
+
+            // Assert
+            Assert.True(subscriptionManager.IsEmpty);
+        }
+
+        #endregion
+    }
+}
